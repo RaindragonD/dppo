882e45e9e4e241b8a2a06fd5cc8b624c2031531c
raindragon <xlfdlyl@gmail.com>
2024-10-20 15:21:25 -0400

    configs

--- launch.sh
+++ launch.sh
@@ -1,4 +1,5 @@
 python script/run.py --config-name=eval_diffusion_mlp --config-dir=cfg/robomimic/eval/square
+python script/run.py --config-name=eval_diffusion_mlp --config-dir=cfg/dynamics
 
 python script/run.py --config-name=pre_diffusion_mlp --config-dir=cfg/robomimic/pretrain/square
 python script/run.py --config-name=pre_diffusion_state_mlp --config-dir=cfg/robomimic/pretrain/square
@@ -9,6 +10,11 @@ python script/run.py --config-name=ft_ppo_diffusion_mlp --config-dir=cfg/robomim
 
 python script/run.py --config-name=ft_diffusion_mlp --config-dir=cfg/dynamics
 python script/run.py --config-name=pre_dynamics_mlp --config-dir=cfg/dynamics
+python script/run.py --config-name=pre_inverse_dynamics_mlp --config-dir=cfg/dynamics
+
 
 python script/dataset/process_robomimic_dataset.py --load_path ~/bc/diffusion_policy/data/robomimic/datasets/square/ph/image.hdf5 --save_dir data/robomimic/square-img-ph --cameras agentview --normalize
 python script/dataset/process_robomimic_dataset.py --load_path ~/bc/diffusion_policy/data/robomimic/datasets/square/ph/image.hdf5 --save_dir data/robomimic/square-ph --normalize
+
+python script/dataset/process_robomimic_dataset_multi.py --load_path ~/bc/diffusion_policy/data/robomimic/datasets/square/ph/image.hdf5 --save_dir data/robomimic/square-ph --normalize
+python script/dataset/process_robomimic_dataset_multi.py --load_paths ~/bc/diffusion_policy/data/robomimic/datasets/square/ph/image.hdf5 ~/bc/diffusion_policy/data/robomimic/datasets/can/ph/image.hdf5 ~/bc/diffusion_policy/data/robomimic/datasets/lift/ph/image.hdf5 --save_dir data/robomimic/multi-ph --normalize
